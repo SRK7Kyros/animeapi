c529c64b1c36fe45f05562737f4aa65ff4df2478
SRK7Kyros <srk7kyros@gmail.com>
2023-06-14 14:05:18 +0200

    s

--- src/lib.rs
+++ src/lib.rs
@@ -18,29 +18,17 @@ pub async fn get_driver() -> AHResult<WebDriver> {
 pub async fn start_geckodriver() -> AHResult<()> {
     println!("start_geckodriver ran");
     let sender = Client::new();
+    println!("got here");
 
     let url = "http://127.0.0.1:4444/status".parse::<hyper::Uri>()?;
+    println!("got here");
 
     let res = sender.get(url).await?.status();
 
     println!("{}", res);
 
-    let f = spawn(async move {
-        let output = std::process::Command::new("/Users/giulio/Desktop/geckodriver").output();
-
-        if output.is_err() {
-            return Ok(());
-        }
-
-        let output = output?;
-
-        let stderr = String::from_utf8(output.stderr)?;
-
-        if stderr.contains("error") {
-            println!("{stderr}");
-        }
-
-        Ok::<(), AHError>(())
+    spawn(async move {
+        std::process::Command::new("/Users/giulio/Desktop/geckodriver").output();
     });
     Ok(())
 }
